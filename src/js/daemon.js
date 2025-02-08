0868566eed730a59080432923459e7d4760bc479
Greon68 <greon-ya@yandex.ru>
2025-02-08 19:23:28 +0300

    commit-2

--- src/js/daemon.js
+++ src/js/daemon.js
@@ -1,4 +1,11 @@
 import Character from "./character"
+/* РЕЦЕНЗИЯ */
+// В дочерних классах переопределять эти методы (levelUp и damage) не нужно.
+// Вы можете их полностью удалить, чтобы реализация полностью бралась из родителя…
+// А так же, в конструкторах, вызов super(name,type,health,level,attack=40,defence=10);
+// не совсем корректный. В JavaScript нет именованных параметров. Вам следовало просто 
+// передавать значения: super(name,type,health,level,40,10);,
+// да и в параметрах конструктора атака и защита не используются, так что, их стоит удалить
 
 export default class  Daemon extends Character {
     constructor(name,type,health,level,attack,defence){
